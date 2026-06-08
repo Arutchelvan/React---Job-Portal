6c08c64d7809246ab70e8cb8173b0639ac8dbf44
Arutchelvan <arulbalaji03@gmail.com>
2026-06-08 21:01:33 +0530

    Single Job Page Added

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,3 +1,40 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { MoonLoader } from "react-spinners";
+
 export default function JobPage() {
-  return <div></div>;
+  const { id } = useParams();
+  const [job, setJob] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getJob() {
+      try {
+        const res = await fetch(`/api/jobs/${id}`);
+        const data = await res.json();
+        console.log(data);
+        setJob(data);
+      } catch (error) {
+        console.log("Error on data fetching", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getJob();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <MoonLoader />
+      ) : (
+        <div>
+          <h1>Job Title: {job.title}</h1>
+          <p>Job Type : {job.type}</p>
+          <p>Job Location : {job.location}</p>
+          <p>Job Description : {job.description}</p>
+        </div>
+      )}
+    </div>
+  );
 }
